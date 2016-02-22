7ec96fe31853f0c293c0f26fd40b3aea763e4992
Vamour Valentin <vamour@a13p16.fil.univ-lille1.fr>
2016-02-22 11:11:34 +0100

    Ajout de la configuration du proxy

---
+++ includes/proxy.php
@@ -0,0 +1,9 @@
+<?php
+    $configContext = array(
+            'http' => array(
+                    'proxy' => 'tcp://cache.univ-lille1.fr:3128',
+                    'request_fulluri' => true
+            )
+    );
+    stream_context_set_default($configContext);
+?>
