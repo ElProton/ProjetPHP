65989a638a82e5715aa557d037f312118c7010d5
Lefebvre Ryan <lefebvrer@a13p15.fil.univ-lille1.fr>
2016-02-22 11:46:06 +0100

    ini fichier projet

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+window.addEventListener("load", dessinerCarte);
+
+function dessinerCarte(){ 
+	var map = L.map('cartecampus').setView([50.6, 3.13], 16);
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '©️ <a href="http://osm.org/copyright">OpenStreetMap>/a> contributors'
+    }).addTo(map);
+    var marker = L.marker([50.609614, 3.136635]).addTo(map)
+       .bindPopup('Le bâtiment M5 <p>Formations en Informatique</p> ...');
