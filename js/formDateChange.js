64f9ab65a1661ebe12622e135c11310faca26e19
ElProton <lefebvreryan@hotmail.com>
2016-03-01 21:54:33 +0000

    evenement lié au mois, année bisextile pas encore pris en charge

---
+++ js/formDateChange.js
@@ -0,0 +1,67 @@
+function init(){
+    var Date = document.getElementsByClassName('dateChange')
+
+    Date[0].addEventListener("change", changeDay);
+    Date[1].addEventListener("change", changeDay);
+}
+
+
+
+function changeDay(event){
+    var Date = document.getElementsByClassName('dateChange');
+    var mois = Date[0];
+    var annee = Date[1];
+
+    var choiceMois = mois.selectedIndex;
+
+    var mounths = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+    if(choiceMois != null) {
+        var jour = document.getElementById("modificationDate");
+        console.log(jour);
+        if (choiceMois == 0 || choiceMois == 2 || choiceMois == 4) {
+            var dayoptions = document.getElementsByClassName("dayOptions");
+            if (dayoptions.length != 0) {
+                for (var i = 0; i <= dayoptions.length; i += 1) {
+                    console.log(dayoptions);
+                    console.log(dayoptions[parseInt(i, 10)]);
+                    document.getElementById("modificationDate").removeChild(dayoptions[parseInt(i, 10)]);
+                }
+            }
+            for (var i = 29; i <= 31; i++) {
+                var option = new Option(i, i);
+                option.className = "dayOptions";
+                jour.appendChild(option);
+            }
+        }
+        else {
+            if (choiceMois == 1) {
+                var dayoptions = document.getElementsByClassName("dayOptions");
+                for (var i = 0; i < dayoptions.length; i) {
+                    console.log(dayoptions);
+                    console.log(dayoptions[parseInt(i, 10)]);
+                    jour.removeChild(dayoptions[parseInt(i, 10)]);
+                }
+            }
+
+            else {
+                var dayoptions = document.getElementsByClassName("dayOptions");
+                for (var i = 0; i <= dayoptions.length; i += 1) {
+                    console.log(dayoptions);
+                    console.log(dayoptions[parseInt(i, 10)]);
+                    document.getElementById("modificationDate").removeChild(dayoptions[parseInt(i, 10)]);
+                }
+                for (var i = 29 ; i <= 30; i++) {
+                    var option = new Option(i, i);
+                    option.className = "dayOptions";
+                    jour.appendChild(option);
+                }
+            }
+        }
+    }
+
+
+}
+
+window.addEventListener("load", init);
+
+
