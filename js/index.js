9d438ac1c1c1e7c6dc082bd4f69742b5e7c5f16e
Firhos <valentin.vamour@gmail.com>
2016-03-01 15:13:23 +0100

    Ajout de tous les marqueurs sur la carte, bouton pour le selectionner dans le formaulaire

--- js/index.js
+++ js/index.js
@@ -8,14 +8,14 @@ function dessinerCarte() {
     }).addTo(map);
     
     var l = document.querySelectorAll("#stations option");
-    console.log("Ok");
     for(var i=0 ; i < l.length ; i++)
     {
-        console.log("Ok");
         var nom = l[i].textContent;
         var insee = l[i].dataset.insee;
+        var txt = nom + "<br/><button class='mapButton' value=\""+insee+"\">Choisir</button>";
+        var marker = addMarker([l[i].dataset.lat, l[i].dataset.lon], map, txt);
+        map.on("popupopen",iChooseYou);
         
-        addMarker([l[i].dataset.lon, l[i].dataset.lat], map);
     }
     
     
@@ -26,10 +26,40 @@ function dessinerCarte() {
   * 
   * @param positionArray An array with two elements: x position and y position
   * @param m The Leaflet map
+  * @param txt A text to bind to the popup that appear when you click on a marker
   * @return A marker
   */
-function addMarker(positionArray, m)
+function addMarker(positionArray, m, txt)
 {
-    var marker = L.marker(positionArray).addTo(m);
+    var marker = L.marker(positionArray).addTo(m)
+    .bindPopup(txt);
     return marker;    
+}
+
+/**
+ * Add an event listener of the popup button of the map.
+ * 
+ * @return None
+ * 
+ */
+function iChooseYou(event)
+{
+    var noeudPopup = event.popup._contentNode;
+    var bouton = noeudPopup.querySelector("button");
+    bouton.addEventListener("click", modifieSelect);
+}
+
+/**
+ * Change the selected station, this function is executed when the user click on the "Choose" button
+ * of a map popup
+ * 
+ * @return None
+ * 
+ */
+function modifieSelect(event)
+{
+    var query = "#stations option[data-insee='"+this.value+"']";
+    var selectOptions = document.querySelectorAll(query);
+    alert(query);
+    selectOptions.selected = true;
 }
