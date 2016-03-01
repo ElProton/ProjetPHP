dac08342d5ab72ba0bebad889c9cda4f6a048467
Firhos <valentin.vamour@gmail.com>
2016-03-01 20:35:46 +0100

    Changement de l'attribut value des options par le num. INSEE

--- includes/formulaireJour.php
+++ includes/formulaireJour.php
@@ -14,7 +14,7 @@
     $stationArray = fgetcsv( $csvFile, 1000, ";");
     
 	while ($stationArray != NULL){
-		echo "<option data-insee='".$stationArray[0]."' data-lon='".$stationArray[3]."' data-lat='".$stationArray[2]."' value='".$stationArray[1]."' > ".$stationArray[1]."</option>";
+		echo "<option data-insee='".$stationArray[0]."' data-lon='".$stationArray[3]."' data-lat='".$stationArray[2]."' value='".$stationArray[0]."' > ".$stationArray[1]."</option>";
 		$stationArray = fgetcsv( $csvFile, 1000, ";");
 	}
 	echo " </select> <br />";
--- includes/traitement.php
+++ includes/traitement.php
@@ -3,7 +3,22 @@
     {
         if(isset($_POST['submit_jour']))
         {
-            
+            if(isset($_POST['day']) 
+            && isset($_POST['mounth']) 
+            && isset($_POST['year'])
+            && isset($_POST['stations']))
+            {
+                $csvFile = fopen("compress.zlib://https://donneespubliques.meteofrance.fr/donnees_libres/Txt/Synop/Archive/synop.".$_POST['year'].$_POST['mounth'].".csv.gz","r");
+                $infoArray = fgetcsv( $csvFile, 1000, ";");
+                $infoArray = fgetcsv( $csvFile, 1000, ";");
+                $found = false;
+                
+  
+            }
+            else
+            {
+                echo "<p>Veuillez faire une recherche dans le formulaire de gauche.</p>";
+            }
         }
         else
         {
