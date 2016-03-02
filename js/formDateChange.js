571cb90fbedb9befc30eea832dee545774e46f40
ElProton <lefebvreryan@hotmail.com>
2016-03-02 11:35:28 +0000

    fin du traitement des jours selon le mois et les années bisextile

--- js/formDateChange.js
+++ js/formDateChange.js
@@ -1,8 +1,31 @@
 function init(){
+    var jour = document.getElementById("modificationDate");
+
+    for (var i = 29; i <= 31; i++) {
+        var option = new Option(i, i);
+        option.className = "dayOptions";
+        jour.appendChild(option);
+    }
+
     var Date = document.getElementsByClassName('dateChange')
 
     Date[0].addEventListener("change", changeDay);
     Date[1].addEventListener("change", changeDay);
+}
+
+function isBis(year) {
+    if ((year%4) == 0){
+        if ((year%100) == 0){
+            if ((year%400) == 0){
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 
@@ -13,8 +36,10 @@ function changeDay(event){
     var annee = Date[1];
 
     var choiceMois = mois.selectedIndex;
+    var choiceannee = annee.selectedIndex;
 
     var mounths = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+    var bisyears = [""]
     if(choiceMois != null) {
         var jour = document.getElementById("modificationDate");
         if (choiceMois == 0 || choiceMois == 2 || choiceMois == 4 || choiceMois == 6 ||choiceMois == 7 || choiceMois == 9 || choiceMois == 11){
@@ -36,6 +61,12 @@ function changeDay(event){
                 for (var i = 0; i < dayoptions.length; i) {
                     jour.removeChild(dayoptions[parseInt(i, 10)]);
                 }
+                if (isBis(annee.options[choiceannee].value, parseInt()) == true){
+                    var option = new Option(29, 29);
+                    option.className = "dayOptions";
+                    jour.appendChild(option);
+                }
+
             }
 
             else {
